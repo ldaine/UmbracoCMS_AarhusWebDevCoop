fd339576b6e34b9cf00f7a42923a3010283a8bd0
ldaine <liga.daine@gmail.com>
2016-03-18 09:32:40 +0100

    working gulp sass watcher

--- AarhusWebDevCoop/gulpfile.js
+++ AarhusWebDevCoop/gulpfile.js
@@ -1,7 +1,11 @@
 ﻿var gulp = require('gulp');
 var sass = require('gulp-sass');
-gulp.task('sass', function(){
-    return gulp.src('scss/style.scss')
+gulp.task('sass', function () {
+    return gulp.src('sass/style.scss')
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
-    .pipe(gulp.dest('css/'))
+    .pipe(gulp.dest('css'))
+});
+
+gulp.task('watch', function () {
+    gulp.watch('sass/**/*.scss', ['sass']);
 });
