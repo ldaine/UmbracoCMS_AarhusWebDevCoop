a0adbe7002158461fcd0fc52e375a96a22ed6f7f
ldaine <liga.daine@gmail.com>
2016-03-18 10:40:22 +0100

    exercise I - IV:  contact page form layout

---
+++ AarhusWebDevCoop/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,19 @@
+﻿using AarhusWebDevCoop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace AarhusWebDevCoop.Controllers
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        // GET: ContactFormSurface
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactForm());
+        }
+    }
+}
