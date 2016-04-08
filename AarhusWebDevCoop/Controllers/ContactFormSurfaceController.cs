466aeb2b67288451919e4d9e704bf6b46ed209ff
ldaine <liga.daine@gmail.com>
2016-04-08 12:21:33 +0200

    add files

---
+++ AarhusWebDevCoop/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,76 @@
+﻿using AarhusWebDevCoop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using Umbraco.Core.Models;
+
+namespace AarhusWebDevCoop.Controllers
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+
+        // GET: ContactFormSurface
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactForm());
+        }
+
+        [HttpPost]
+        public ActionResult HandleFormSubmit(ContactForm model)
+        {
+            if (!ModelState.IsValid) { return CurrentUmbracoPage(); }
+
+            MailMessage message = new MailMessage();
+            message.To.Add("cqtkbnns@gmail.com");
+            message.Subject = model.Subject;
+            message.From = new MailAddress(model.Email, model.Name);
+            message.Body = model.Message;
+
+            using (SmtpClient smtp = new SmtpClient())
+            {
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.UseDefaultCredentials = false;
+                smtp.EnableSsl = true;
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.Credentials = new System.Net.NetworkCredential("cqtkbnns@gmail.com", "yquqdmowgzpfkxpv");
+                smtp.EnableSsl = true;
+                // send mail
+                try
+                {
+                    smtp.Send(message);
+                    TempData["success"] = true;
+                }
+                catch (SmtpException ex)
+                {
+                    var Message = ex.Message.ToString();
+                    // ex.FailedRecipient and ex.GetBaseException() should give you enough info.
+                }
+
+            }
+            
+            //saving the message
+
+            IContent comment = Services.ContentService.CreateContent(model.Subject, CurrentPage.Id, "messages");
+
+            comment.SetValue("vmname", model.Name);
+            comment.SetValue("email", model.Email);
+            comment.SetValue("subject", model.Subject);
+            comment.SetValue("message", model.Message);
+
+            //save
+            Services.ContentService.Save(comment);
+
+            return RedirectToCurrentUmbracoPage();
+
+
+        }
+
+
+
+    }
+}
---
+++ AarhusWebDevCoop/gulpfile.js
@@ -0,0 +1,11 @@
+﻿var gulp = require('gulp');
+var sass = require('gulp-sass');
+gulp.task('sass', function () {
+    return gulp.src('sass/style.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(gulp.dest('css'))
+});
+
+gulp.task('watch', function () {
+    gulp.watch('sass/**/*.scss', ['sass']);
+});
